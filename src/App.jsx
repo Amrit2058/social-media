2e6415e46f4ab7ca64d07bf7fc6e0739cf34ceef
Amrit Adhikari <ilovegrapes100@gmail.com>
2025-03-25 22:40:12 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import CreatePost from "./components/CreatePost";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import PostList from "./components/PostList";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+  return (
+    <div className="app-container">
+      <Sidebar />
+      <div className="content">
+        <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
